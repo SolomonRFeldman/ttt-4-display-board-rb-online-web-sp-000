a2435fdd430e8d6358b6b61e80d99555c18665b6
affable-motherboard-2531 <solomonrfeldman@gmail.com>
2019-03-20 10:02:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board
-  puts " #{0}  | #{0}  | #{0}  "
+  puts " #{board(0)}  | #{0}  | #{0}  "
   puts "-----------"
   puts " #{0}  | #{0}  |   "
   puts "-----------"
